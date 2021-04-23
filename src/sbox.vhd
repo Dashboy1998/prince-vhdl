27cd2e27ae810ec4088262f483fa7bf224dea0a2
Carlos M. Martinez <Dashboy1998@gmail.com>
2021-04-23 00:47:43 -0400

    Added gates to sbox

--- src/sbox.vhd
+++ src/sbox.vhd
@@ -57,20 +57,74 @@ begin
 end architecture dataflow;
 
 architecture structural of sbox is
+	component not1
+		port( 
+		A: in std_logic;
+		Z: out std_logic 
+		);
+	end component not1; 
+	component and2
+		port( 
+		A, B: in std_logic;
+		Z: out std_logic 
+		);
+	end component and2;
+	component and3
+		port( 
+		A, B, C: in std_logic;
+		Z: out std_logic 
+		);
+	end component and3;
+	component or3
+		port( 
+		A, B, C: in std_logic;
+		Z: out std_logic 
+		);
+	end component or3;
+	component or5
+		port( 
+		A, B, C, D, E: in std_logic;
+		Z: out std_logic 
+		);
+	end component or5;
 	alias A is data_in(3);
 	alias B is data_in(2);
 	alias C is data_in(1);
 	alias D is data_in(0);
 	signal nA, nB, nC, nD: std_logic;
+	signal nAnC, BnD, ACnD, nCD, AnC, AB, nAnB, nBnC, nCnD, nAnBnC, nAnBnD, nABC, BCnD, AnCD: std_logic; -- AND Gate
+	signal nAnC_BnD_ACnD, nCD_AnC_AB, nAnB_nBnC_nCnD, nAnBnC_nAnBnD_nABC_BCnD_AnCD: std_logic; -- OR Gate
 begin 
-	nA <= not A;
-	nB <= not B;
-	nC <= not C;
-	nD <= not D;
-	
-	data_out(3) <= (nA and nC) or (B and nD) or (A and C and nD);
-	data_out(2) <= (nC and D) or (A and nC) or (A and B);
-	data_out(1) <= (nA and nB) or (nB and nC) or (nC and nD);
-	data_out(0) <= (nA and nB and nC) or (nA and nB and nD) or (nA and B and C) or (B and C and nD) or (A and nC and D);
-	
+	N3: not1 port map(A,nA);
+	N2: not1 port map(B,nB);
+	N1: not1 port map(C,nC);
+	N0: not1 port map(D,nD);
+
+	-- Products of dataout(3)
+	AND_nAnC: and2 port map(nA,nC,nAnC);
+	AND_BnD: and2 port map(B,nD,BnD);
+	AND_ACnD: and3 port map(A,C,nD,ACnD);
+
+	-- Products of dataout(2)
+	AND_nCD: and2 port map(nC,D,nCD);
+	AND_AnC: and2 port map(A,nC,AnC);
+	AND_AB: and2 port map(A,B,AB);
+
+	-- Products of dataout(1)
+	AND_nAnB: and2 port map(nA,nB,nAnB);
+	AND_nBnC: and2 port map(nB,nC,nBnC);
+	AND_nCnD: and2 port map(nC,nD,nCnD);
+
+	-- Products of dataout(0)
+	AND_nAnBnC: and3 port map(nA,nB,nC,nAnBnC);
+	AND_nAnBnD: and3 port map(nA,nB,nD,nAnBnD);
+	AND_nABC: and3 port map(nA,B,C,nABC);
+	AND_BCnD: and3 port map(B,C,nD,BCnD);
+	AND_AnCD: and3 port map(A,nC,D,AnCD);
+
+	-- Sum of Products
+	D3: or3 port map(nAnC,BnD,ACnD,data_out(3));
+	D2:	or3 port map(nCD,AnC,AB,data_out(2));
+	D1:	or3 port map(nAnB,nBnC,nCnD,data_out(1));
+	D0: or5 port map(nAnBnC,nAnBnD,nABC,BCnD,AnCD,data_out(0));
 end architecture structural;
